a311ae2a4b62ac54003915f6f9ad02e9eba29a67
alexzv08 <alexzv8@gmail.com>
2025-05-13 11:04:19 +0200

    fix: Remove CORS configuration and autoConnect option from socket initialization

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -1,11 +1,7 @@
 // src/utils/socket.js
 import { io } from 'socket.io-client';
 const socket = io(import.meta.env.VITE_BACKEND_URL, {
-    autoConnect: false,
-    cors: {
-        origin: 'https://tcg-gundam-game-1.onrender.com',
-        methods: ['GET', 'POST'],
-        credentials: true
-    }
+    transports: ["websocket"], // Forzar el uso de WebSocket
+    withCredentials: true,
 });
 export default socket;
